4425ef0d9d1839c8273bee8e0315931f6e9d7af2
Brian Sandberg <bsandberg@ontogenesis.com>
2019-11-02 15:41:42 +0100

    Queryable avatars.

---
+++ PlayTogetherApi.Web/GraphQl/Types/BuiltinAvatarType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using GraphQL.Types;
+using PlayTogetherApi.Domain;
+using PlayTogetherApi.Web.Models;
+
+namespace PlayTogetherApi.Web.GraphQl.Types
+{
+    public class BuiltinAvatarType : ObjectGraphType<BuiltinAvatar>
+    {
+        public BuiltinAvatarType(PlayTogetherDbContext db, IConfiguration config)
+        {
+            Field("id", avatar => avatar.AvatarId, type: typeof(IntGraphType));
+            Field("path", avatar => avatar.ImagePath, type: typeof(StringGraphType));
+            Field("image", avatar => config.GetValue<string>("AssetPath") + avatar.ImagePath, type: typeof(StringGraphType));
+        }
+    }
+}
