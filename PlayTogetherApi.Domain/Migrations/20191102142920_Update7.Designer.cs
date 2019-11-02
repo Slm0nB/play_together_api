ec1bcee7461ccbf3b9b6129b4edb7c0d2e642624
Brian Sandberg <bsandberg@ontogenesis.com>
2019-11-02 15:35:45 +0100

    BuiltinAvatar table.

--- PlayTogetherApi.Domain/Migrations/20191102142920_Update7.Designer.cs
+++ PlayTogetherApi.Domain/Migrations/20191102142920_Update7.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PlayTogetherApi.Domain;
@@ -9,9 +10,10 @@ using PlayTogetherApi.Domain;
 namespace PlayTogetherApi.Domain.Migrations
 {
     [DbContext(typeof(PlayTogetherDbContext))]
-    partial class PlayTogetherDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191102142920_Update7")]
+    partial class Update7
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
