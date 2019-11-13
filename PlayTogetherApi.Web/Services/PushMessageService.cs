66df6c7fdd01f197921b207041947c130c0e3a47
Brian Sandberg <bsandberg@ontogenesis.com>
2019-11-13 23:51:33 +0100

    WIP Pushmessage service.

---
+++ PlayTogetherApi.Web/Services/PushMessageService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayTogetherApi.Services
+{
+    public class PushMessageService
+    {
+        public async Task PushMessage<T>(string name, string title, string body, T payload = null, params string[] recipients) where T : class
+        {
+            var postbody = new
+            {
+                notification_content = new
+                {
+                    name,
+                    title,
+                    body,
+                    custom_data = payload,
+                    notification_target = new {
+                        Type = "user_ids_target",
+                        user_ids = recipients
+                    }
+                }
+            };
+
+            using (var client = new HttpClient())
+            {
+                var tokenResponseMessage = await client.SendAsync(new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri("https://appcenter.ms/api/v0.1/apps/SimonBrettschneider/Lets-play-together/push/notifications"),
+                    Headers = {
+                        {"X-API-Token", ""}, // todo: insert key
+                        {"Accept", "application/json"}
+                    },
+                    Content = new StringContent("{}", Encoding.UTF8, "application/json")
+                });
+            }
+        }
+    }
+}
