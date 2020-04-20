9dae1fbd3f24daca1a1fe1904a94ac3b033933f7
Brian Sandberg <bsandberg@ontogenesis.com>
2020-04-20 23:45:37 +0200

    WIP events query.

--- PlayTogetherApi.Web/Services/EventsQueryService.cs
+++ PlayTogetherApi.Web/Services/EventsQueryService.cs
@@ -10,61 +10,77 @@ namespace PlayTogetherApi.Web.Services
     {
         public Guid? UserId;
         public List<Guid> FriendIds;
+        public Guid[] CreatedByUserIds;
+        public Guid[] GameIds;
 
-        public IQueryable<Event> ProcessFilter(IQueryable<Event> query, Guid userId, List<Guid> friendIds, bool onlyPrivateFilter, bool onlyByFriendsFilter, bool onlyJoinedFilter, Guid[] onlyByUserIds, Guid[] onlyGameIds)
+        public bool OnlyPrivateFilter;
+        public bool OnlyByFriendsFilter;
+        public bool OnlyByUsersFilter;
+        public bool OnlyGamesFilter;
+        public bool OnlyJoinedFilter;
+
+        public bool IncludePrivateFilter;
+        public bool IncludeByFriendsFilter;
+        public bool IncludeByUsersFilter;
+        public bool IncludeGamesFilter;
+        public bool IncludeJoinedFilter;
+
+        IQueryable<Event> ProcessExclusiveFilter(IQueryable<Event> query)
         {
-            friendIds = friendIds ?? new List<Guid>();
-            onlyByFriendsFilter = onlyByFriendsFilter && friendIds.Any();
-
-            onlyByUserIds = onlyByUserIds ?? new Guid[0];
-            var onlyByUsersFilter = onlyByUserIds.Any();
-
-            onlyGameIds = onlyGameIds ?? new Guid[0];
-            var onlyGamesFilter = onlyGameIds.Any();
-
-            bool anyFilter = onlyPrivateFilter || onlyByFriendsFilter || onlyJoinedFilter || onlyByUsersFilter || onlyGamesFilter;
-            if (!anyFilter)
-                return query;
-
-            /*
-            if (onlyPrivateFilter)
+            if (OnlyPrivateFilter)
             {
                 query = query.Where(n => n.FriendsOnly == true);
             }
 
-            if (onlyByFriendsFilter)
+            if (OnlyByFriendsFilter)
             {
-                query = query.Where(n => friendIds.Contains(n.CreatedByUserId));
+                query = query.Where(n => FriendIds.Contains(n.CreatedByUserId));
             }
 
-            if (onlyByUsersFilter)
+            if (OnlyByUsersFilter && CreatedByUserIds != null && CreatedByUserIds.Any())
             {
-                var first = onlyByUserIds.First();
-                query = onlyByUserIds.Count() == 1
+                var first = CreatedByUserIds.First();
+                query = CreatedByUserIds.Count() == 1
                          ? query.Where(n => n.CreatedByUserId == first)
-                         : query.Where(n => onlyByUserIds.Contains(n.CreatedByUserId));
+                         : query.Where(n => CreatedByUserIds.Contains(n.CreatedByUserId));
             }
 
-            if (onlyGamesFilter)
+            if (OnlyGamesFilter && GameIds != null && GameIds.Any())
             {
-                var first = onlyGameIds.First();
-                query = onlyGameIds.Count() == 1
+                var first = GameIds.First();
+                query = GameIds.Count() == 1
                          ? query.Where(n => n.GameId == first)
-                         : query.Where(n => n.GameId.HasValue && onlyGameIds.Contains(n.GameId.Value));
+                         : query.Where(n => n.GameId.HasValue && GameIds.Contains(n.GameId.Value));
             }
 
-            if (onlyJoined)
+            if (OnlyJoinedFilter)
             {
-                query = query.Where(n => n.Signups.Any(nn => nn.UserId == userId));
+                query = query.Where(n => n.Signups.Any(nn => nn.UserId == UserId));
             }
-            */
+
+            return query;
+        }
+
+        IQueryable<Event> ProcessInclusiveFilter(IQueryable<Event> query)
+        {
+            FriendIds = FriendIds ?? new List<Guid>();
+            CreatedByUserIds = CreatedByUserIds ?? Array.Empty<Guid>();
+            GameIds = GameIds ?? Array.Empty<Guid>();
+
+            bool actualJoinedFilter = IncludeJoinedFilter && UserId.HasValue;
+            bool actualByUsersFilter = IncludeByUsersFilter && CreatedByUserIds.Any();
+            bool actualGamesFilter = IncludeGamesFilter && GameIds.Any();
+
+            bool anyFilter = IncludePrivateFilter || IncludeByFriendsFilter || actualByUsersFilter || actualByUsersFilter || actualGamesFilter;
+            if (!anyFilter)
+                return query;
 
             query = query.Where(n =>
-                (onlyPrivateFilter && n.FriendsOnly)||
-                (onlyByFriendsFilter && friendIds.Contains(n.CreatedByUserId))  ||
-                (onlyJoinedFilter && n.Signups.Any(nn => nn.UserId == userId)) ||
-                (onlyByUsersFilter && onlyByUserIds.Contains(n.CreatedByUserId)) ||
-                (onlyGamesFilter && n.GameId.HasValue && onlyGameIds.Contains(n.GameId.Value))
+                (OnlyPrivateFilter && n.FriendsOnly)||
+                (IncludeByFriendsFilter && FriendIds.Contains(n.CreatedByUserId))  ||
+                (actualJoinedFilter && n.Signups.Any(nn => nn.UserId == UserId)) ||
+                (actualByUsersFilter && CreatedByUserIds.Contains(n.CreatedByUserId)) ||
+                (actualGamesFilter && n.GameId.HasValue && GameIds.Contains(n.GameId.Value))
             );
 
             return query;
@@ -105,7 +121,7 @@ namespace PlayTogetherApi.Web.Services
             return query;
         }
 
-        IQueryable<Event> Process(IQueryable<Event> query)
+        public IQueryable<Event> Process(IQueryable<Event> query)
         {
             if (UserId.HasValue)
             {
@@ -118,6 +134,8 @@ namespace PlayTogetherApi.Web.Services
                 query = query.Where(n => !n.FriendsOnly);
             }
 
+            query = ProcessExclusiveFilter(query);
+            query = ProcessInclusiveFilter(query);
 
             return query;
         }
