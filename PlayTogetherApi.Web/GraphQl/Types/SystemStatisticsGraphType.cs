7484011ed45c2bdc492d34a0c222c6286421b90c
Brian Sandberg <bsandberg@ontogenesis.com>
2020-05-08 11:45:34 +0200

    System-query for getting the count of active userstatistics-subscriptions.

---
+++ PlayTogetherApi.Web/GraphQl/Types/SystemStatisticsGraphType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.Types;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PlayTogetherApi.Data;
+using PlayTogetherApi.Services;
+using PlayTogetherApi.Web.Models;
+
+namespace PlayTogetherApi.Web.GraphQl.Types
+{
+    public class SystemStatisticsGraphType : ObjectGraphType
+    {
+        public SystemStatisticsGraphType(ObservablesService observablesService)
+        {
+            Name = "SystemStatistics";
+
+            Field<IntGraphType>("userStatisticsSubscriptionCount", resolve: context => observablesService.UserStatisticsStreams.Count() );
+
+        }
+    }
+}
