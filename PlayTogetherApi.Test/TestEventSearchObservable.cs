8f1558604a92c28119eca66c42271c0c74108afa
Brian Sandberg <bsandberg@ontogenesis.com>
2020-06-18 23:31:25 +0200

    WIP eventsearch test.

--- PlayTogetherApi.Test/TestEventSearchObservable.cs
+++ PlayTogetherApi.Test/TestEventSearchObservable.cs
@@ -28,7 +28,7 @@ namespace PlayTogetherApi.Test
         }
 
         [TestMethod]
-        public async Task TestEventSearch()
+        public async Task TestEventSearch_UserAddingEvent()
         {
             IDisposable sub1 = null;
             try
@@ -42,18 +42,14 @@ namespace PlayTogetherApi.Test
                     await MockData.PopulateDbAsync(db, true);
 
                     var testUser = MockData.Users[0];
-                    var friendUser = MockData.Users[1];
 
-                    await interactions.ChangeUserRelationAsync(testUser.UserId, friendUser.UserId, UserRelationAction.Invite);
-                    await interactions.ChangeUserRelationAsync(friendUser.UserId, testUser.UserId, UserRelationAction.Accept);
-
+                    // subscribe to events that the user has joined
                     var queryService = new EventsQueryService
                     {
                         UserId = testUser.UserId,
-                        FriendIds = new List<Guid> { friendUser.UserId },
                         StartsAfterDate = DateTime.Today.AddDays(-1),
                         IncludeJoinedFilter = true,
-                        IncludeByUsersFilter = new [] { testUser.UserId }
+                        IncludeByUsersFilter = new[] { testUser.UserId } // TODO: THIS SHOULDN'T BE NECESSARY, SINCE THE USER AUTOMATICALLY JOINS THE EVENT!
                     };
 
                     var searchObservable = new EventSearchObservable(observables, queryService, new List<Event>());
@@ -70,34 +66,79 @@ namespace PlayTogetherApi.Test
                         ecm1 = ecm;
                     });
 
+
                     var newEvent = await interactions.CreateEventAsync(testUser.UserId, DateTime.UtcNow.AddHours(2), DateTime.UtcNow.AddHours(3), "testevent1", "", false, MockData.Games[0].GameId);
 
                     Assert.IsNotNull(searchUpdate);
                     Assert.IsNotNull(ecm1);
 
 
-                    // todo: verify queryservice also emitted it
+                    // todo: leave the event, and verify that we get an event-removed update
+                }
+            }
+            finally
+            {
+                sub1?.Dispose();
+            }
+        }
 
 
 
 
-                    // todo: populate queryService, events joined by friends
+        [TestMethod]
+        public async Task TestEventSearch_FriendAddingEvent()
+        {
+            IDisposable sub1 = null;
+            try
+            {
+                using (var db = di.GetService<PlayTogetherDbContext>())
+                {
+                    var observables = di.GetService<ObservablesService>();
+                    var interactions = di.GetService<InteractionsService>();
+                    interactions.EnablePushMessages = false;
 
-                    // todo: subscripbe to EventSearchObservable
+                    await MockData.PopulateDbAsync(db, true);
 
-                    // todo: move the "join event" logic into the interaction service
+                    var testUser = MockData.Users[0];
+                    var friendUser = MockData.Users[1];
 
-                    // todo: make the friend of the test-user join an event
-                    // todo: validate that the observable added the event, since the friend joined
+                    // setup friends in advance
+                    await interactions.ChangeUserRelationAsync(testUser.UserId, friendUser.UserId, UserRelationAction.Invite);
+                    await interactions.ChangeUserRelationAsync(friendUser.UserId, testUser.UserId, UserRelationAction.Accept);
 
-                    // todo: remove the user-relation
-                    // todo: validate that the observable removed the event since the friend disappeared
+                    var queryService = new EventsQueryService
+                    {
+                        UserId = testUser.UserId,
+                        FriendIds = new List<Guid> { friendUser.UserId },
+                        StartsAfterDate = DateTime.Today.AddDays(-1),
+                        IncludeByFriendsFilter = true
+                    };
 
-                    // todo: add the user-relation
-                    // todo: validate that the observable added the event since the friend was added
+                    var searchObservable = new EventSearchObservable(observables, queryService, new List<Event>());
 
-                    // todo: make the friend leave the event
-                    // todo: observe that the observable removed the event since the friend is no longer joined
+                    EventSearchUpdateModel searchUpdate = null;
+                    searchObservable.AsObservable().Subscribe(esum =>
+                    {
+                        searchUpdate = esum;
+                    });
+
+                    EventChangedModel ecm1 = null;
+                    sub1 = observables.GameEventStream.AsObservable().Subscribe(ecm =>
+                    {
+                        ecm1 = ecm;
+                    });
+
+                    // create event, which should be added to our search
+                    var newEvent = await interactions.CreateEventAsync(friendUser.UserId, DateTime.UtcNow.AddHours(2), DateTime.UtcNow.AddHours(3), "testevent1", "", false, MockData.Games[0].GameId);
+
+                    Assert.IsNotNull(searchUpdate);
+                    Assert.IsNotNull(ecm1);
+
+
+                    // todo: remove the friendship, and verify that we get an event-removed update
+
+
+
 
                 }
             }
@@ -106,5 +147,80 @@ namespace PlayTogetherApi.Test
                 sub1?.Dispose();
             }
         }
+
+
+
+
+
+        [TestMethod]
+        public async Task TestEventSearch_EventWhenBefriendingCreator()
+        {
+            IDisposable sub1 = null;
+            try
+            {
+                using (var db = di.GetService<PlayTogetherDbContext>())
+                {
+                    var observables = di.GetService<ObservablesService>();
+                    var interactions = di.GetService<InteractionsService>();
+                    interactions.EnablePushMessages = false;
+
+                    await MockData.PopulateDbAsync(db, true);
+
+                    var testUser = MockData.Users[0];
+                    var friendUser = MockData.Users[1];
+
+                    // search for events created by our friends
+                    var queryService = new EventsQueryService
+                    {
+                        UserId = testUser.UserId,
+                        FriendIds = new List<Guid> { },
+                        StartsAfterDate = DateTime.Today.AddDays(-1),
+                        //IncludeJoinedFilter = true,
+                        IncludeByFriendsFilter = true
+                    };
+
+                    var searchObservable = new EventSearchObservable(observables, queryService, new List<Event>());
+
+                    EventSearchUpdateModel searchUpdate = null;
+                    searchObservable.AsObservable().Subscribe(esum =>
+                    {
+                        searchUpdate = esum;
+                    });
+
+                    EventChangedModel ecm1 = null;
+                    sub1 = observables.GameEventStream.AsObservable().Subscribe(ecm =>
+                    {
+                        ecm1 = ecm;
+                    });
+
+                    var newEvent = await interactions.CreateEventAsync(friendUser.UserId, DateTime.UtcNow.AddHours(2), DateTime.UtcNow.AddHours(3), "testevent1", "", false, MockData.Games[0].GameId);
+
+                    Assert.IsNull(searchUpdate);
+                    Assert.IsNotNull(ecm1);
+
+                    ecm1 = null;
+
+                    // befriend the user who created the event, which should make the event show up in our search
+                    await interactions.ChangeUserRelationAsync(testUser.UserId, friendUser.UserId, UserRelationAction.Invite);
+                    await interactions.ChangeUserRelationAsync(friendUser.UserId, testUser.UserId, UserRelationAction.Accept);
+
+                    //Assert.IsNotNull(searchUpdate);       todo, will be set once the friendupdate is updated
+                    Assert.IsNull(ecm1);
+
+
+                }
+            }
+            finally
+            {
+                sub1?.Dispose();
+            }
+        }
+
+
+        // testcases:
+        // - testuser subscribes to friend events, friend creates an event, the event should then be added
+        // - testuser subscribes to friend events, unrelated user creates an event, testuser befriends the other user, the event should then be added
+        // - testuser subscribes to friend events, friend creates an event, the event should then be added, friend is unfriended, the event should be removed
+
     }
 }
